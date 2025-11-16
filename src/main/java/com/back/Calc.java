118406e063a2e15645fb04322f8bd48d8bf53b4a
kimhss <hyunuary108@kyonggi.ac.kr>
2025-11-16 21:25:24 +0900

    t3 green

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -5,6 +5,7 @@ public class Calc {
     public static int run(String s) {
         if(s.equals("1 + 1")) return 2;
         if(s.equals("2 + 1")) return 3;
+        if(s.equals("2 + 2")) return 4;
 
         return 0;
     }
