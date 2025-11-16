e49230c65f68d2a598c13703c79535b6a2b366b9
kimhss <hyunuary108@kyonggi.ac.kr>
2025-11-16 21:30:58 +0900

    t4 blue

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -3,12 +3,15 @@ package com.back;
 public class Calc {
 
     public static int run(String s) {
-        if(s.equals("1 + 1")) return 2;
-        if(s.equals("2 + 1")) return 3;
-        if(s.equals("2 + 2")) return 4;
-        if(s.equals("1000 + 280")) return 1280;
-        if(s.equals("2 - 1")) return 1;
+//        if(s.equals("1 + 1")) return 2;
+//        if(s.equals("2 + 1")) return 3;
+//        if(s.equals("2 + 2")) return 4;
+//        if(s.equals("1000 + 280")) return 1280;
+//        if(s.equals("2 - 1")) return 1;
+        String[] str = s.split("\\+");
+        int a = Integer.parseInt(str[0].trim());
+        int b = Integer.parseInt(str[1].trim());
 
-        return 0;
+        return a + b;
     }
 }
