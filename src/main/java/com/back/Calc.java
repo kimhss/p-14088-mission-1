e469925de0e1d60e690d973565b618ca98fa34cb
kimhss <hyunuary108@kyonggi.ac.kr>
2025-11-20 22:14:55 +0900

    t12 GREEN

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -3,26 +3,16 @@ package com.back;
 public class Calc {
 
     public static int run(String s) {
-        // "10 + 20 + 30 == 60"
-
-//        if (s.equals("10 + 20 + 30")) return 60;
-//        if (s.equals("10 - 20 + 30")) return 20;
-
-        if(!s.contains(" ")) return Integer.parseInt(s);
-        String[] str = s.split(" ", 3);
-
-        int a = Integer.parseInt(str[0].trim());
-        String calc = str[1].trim();
-
-        switch (calc) {
-            case "+" -> {
-                return a + run(str[2].trim());
-            }
-            case "-" -> {
-                return a - run(str[2].trim());
-            }
+        if(s.contains("-")) {
+            s = s.replace("- ", "+ -");
         }
 
-        return 0;
+        String[] arr = s.split("\\+");
+        int num = 0;
+        for(String str : arr) {
+            num += Integer.parseInt(str.trim());
+        }
+
+        return num;
     }
 }
