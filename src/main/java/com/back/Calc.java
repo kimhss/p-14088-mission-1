2130c90b7d39ae8d5ea5fcf3f16a8ca486103fb3
kimhss <hyunuary108@kyonggi.ac.kr>
2025-11-20 22:33:46 +0900

    t14 GREEN

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -6,13 +6,23 @@ public class Calc {
         if(s.contains("-")) {
             s = s.replace("- ", "+ -");
         }
+        if(s.contains("*")) {
+            String[] arr = s.split("\\*");
+            int sum = 1;
+            for(String str : arr) {
+                sum *= Integer.parseInt(str.trim());
+            }
 
-        String[] arr = s.split("\\+");
-        int num = 0;
-        for(String str : arr) {
-            num += Integer.parseInt(str.trim());
+            return sum;
+
         }
 
-        return num;
+        String[] arr = s.split("\\+");
+        int sum = 0;
+        for(String str : arr) {
+            sum += Integer.parseInt(str.trim());
+        }
+
+        return sum;
     }
 }
