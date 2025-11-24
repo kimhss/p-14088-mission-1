1f2ac074305b6a999315784b6dd1acfedceefb2d
kimhss <hyunuary108@kyonggi.ac.kr>
2025-11-25 00:00:36 +0900

    t24 green

--- src/test/java/com/back/CalcTest.java
+++ src/test/java/com/back/CalcTest.java
@@ -144,12 +144,12 @@ public class CalcTest {
         assertThat(Calc.run("(10 + 20) * 3")).isEqualTo(90);
     }
 
-//    @Test
-//    @DisplayName("10 + (10 + 5) == 25")
-//    void t24() {
-//        assertThat(Calc.run("10 + (10 + 5)")).isEqualTo(25);
-//    }
-//
+    @Test
+    @DisplayName("10 + (10 + 5) == 25")
+    void t24() {
+        assertThat(Calc.run("10 + (10 + 5)")).isEqualTo(25);
+    }
+
 //    @Test
 //    @DisplayName("-(10 + 5) == -15")
 //    void t25() {
