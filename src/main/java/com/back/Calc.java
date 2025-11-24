decc735ec2b40caf471fa470aa5473bac1e61da3
kimhss <hyunuary108@kyonggi.ac.kr>
2025-11-24 23:10:35 +0900

    t15 green

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -3,24 +3,31 @@ package com.back;
 public class Calc {
 
     public static int run(String s) {
+
+        // 10 + 5 * 2
+
+        // 필수로
         if(s.contains("-")) {
             s = s.replace("- ", "+ -");
         }
-        if(s.contains("*")) {
+
+        if(s.contains("*") && !s.contains("+")) {
             String[] arr = s.split("\\*");
             int sum = 1;
             for(String str : arr) {
                 sum *= Integer.parseInt(str.trim());
             }
-
             return sum;
-
         }
 
-        String[] arr = s.split("\\+");
+        // 10 + 5 * 2
         int sum = 0;
-        for(String str : arr) {
-            sum += Integer.parseInt(str.trim());
+        if(s.contains("+")) {
+            String[] arr = s.split("\\+");
+            for(String str : arr) {
+                if(str.contains("*")) sum += run(str.trim());
+                else sum += Integer.parseInt(str.trim());
+            }
         }
 
         return sum;
