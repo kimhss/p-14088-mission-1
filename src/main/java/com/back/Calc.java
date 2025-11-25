566a10a34a1efdf1aa0cbed017d5d19c83457493
kimhss <hyunuary108@kyonggi.ac.kr>
2025-11-25 21:23:48 +0900

    t25 green

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -3,46 +3,38 @@ package com.back;
 public class Calc {
 
     public static int run(String s) {
-
-
-
         s = s.trim();
+
         // 필수로
         if(s.contains("-")) {
             s = s.replace("- ", "+ -");
         }
 
+        // -(10 + 5)
         // (10 + 20) * 3
         // 괄호 벗기기 + 괄호 먼저 계산
         if(s.contains("(") && s.contains(")")) {
-            if(s.contains("*")) {
+            if (s.contains("*")) {
                 String answer = "";
                 String[] tmp = s.split("\\*");
-                for(String str : tmp) {
+                for (String str : tmp) {
                     str = str.trim();
                     if (str.contains("(") && str.contains(")")) {
                         answer += String.valueOf(run(str.replaceAll("[()]", "")));
-                    }
-                    else answer += " * " + str;
+                    } else answer += " * " + str;
                 }
 
                 return run(answer);
             }
-
+            else if (s.contains("-(")) {
+                s = s.replaceAll("[()]", "");
+                s = s.replace("+ ", "+ -");
+                return run(s);
+            }
             else {
                 String tmp = s.replaceAll("[()]", "");
                 return run(tmp);
             }
-
-        }
-
-        if(s.contains("*") && !s.contains("+")) {
-            String[] arr = s.split("\\*");
-            int sum = 1;
-            for(String str : arr) {
-                sum *= Integer.parseInt(str.trim());
-            }
-            return sum;
         }
 
         // 10 + 5 * 2
@@ -54,6 +46,15 @@ public class Calc {
                 else sum += Integer.parseInt(str.trim());
             }
             return sum;
+        }
+
+        int mul = 1;
+        if(s.contains("*") && s.contains(" ")) {
+            String[] arr = s.split("\\*");
+            for(String str : arr) {
+                mul *= Integer.parseInt(str.trim());
+            }
+            return mul;
         }
 
         return sum;
--- src/test/java/com/back/CalcTest.java
+++ src/test/java/com/back/CalcTest.java
@@ -150,11 +150,11 @@ public class CalcTest {
         assertThat(Calc.run("10 + (10 + 5)")).isEqualTo(25);
     }
 
-//    @Test
-//    @DisplayName("-(10 + 5) == -15")
-//    void t25() {
-//        assertThat(Calc.run("-(10 + 5)")).isEqualTo(-15);
-//    }
+    @Test
+    @DisplayName("-(10 + 5) == -15")
+    void t25() {
+        assertThat(Calc.run("-(10 + 5)")).isEqualTo(-15);
+    }
 //
 //    @Test
 //    @DisplayName("-(8 + 2) * -(7 + 3) + 5 == 105")
