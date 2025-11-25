a3ee9518c5c301af9e5253d2b201cbfe4135f67c
kimhss <hyunuary108@kyonggi.ac.kr>
2025-11-25 23:17:39 +0900

    t27 green

--- src/test/java/com/back/CalcTest.java
+++ src/test/java/com/back/CalcTest.java
@@ -162,11 +162,11 @@ public class CalcTest {
         assertThat(Calc.run("-(8 + 2) * -(7 + 3) + 5")).isEqualTo(105);
     }
 
-//    @Test
-//    @DisplayName("5 - (1 + 5) == -1")
-//    void t27() {
-//        assertThat(Calc.run("5 - (1 + 5)")).isEqualTo(-1);
-//    }
+    @Test
+    @DisplayName("5 - (1 + 5) == -1")
+    void t27() {
+        assertThat(Calc.run("5 - (1 + 5)")).isEqualTo(-1);
+    }
 //
 //    @Test
 //    @DisplayName("3 * 1 + (1 - (4 * 1 - (1 - 1))) == 0")
